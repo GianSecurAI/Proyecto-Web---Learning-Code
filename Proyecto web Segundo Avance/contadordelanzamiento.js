f98f258f2d75e5b5d0e04f2a15458bf8e18cdd6c
Gianmarco <gianmarcomenacho2@gmail.com>
2024-11-22 02:04:27 -0500

    final

--- Proyecto web Segundo Avance/contadordelanzamiento.js
+++
@@ -1,9 +0,0 @@
-// Obtener el contador y el botón
-let contador = 0;
-const contadorElemento = document.getElementById('contador');
-
-// Función para incrementar el contador
-function incrementarContador() {
-  contador++;
-  contadorElemento.textContent = contador; // Actualizar el texto del contador
-}
