789ff6ce5b8e1d6c14e5fedc3cab4995e33c26b2
Gianmarco <gianmarcomenacho2@gmail.com>
2024-11-22 00:53:51 -0500

    Cambios finales

---
+++ Proyecto web Segundo Avance/citas.js
@@ -0,0 +1,38 @@
+let citas = [
+  {
+    'autor': 'Dennis Ritchie',
+    'texto': 'C programación es el lenguaje de la computación. Es como la lengua latina de las ciencias de la computación.'
+  },
+  {
+    'autor': 'Steve Jobs',
+    'texto': 'La innovación es lo que distingue a un líder de un seguidor.'
+  },
+  {
+    'autor': 'Grace Hopper',
+    'texto': 'La programación no es difícil. Es solo cuestión de tomar una decisión y seguir adelante'
+  },
+  {
+    'autor': 'Bill Gates',
+    'texto': 'Tus clientes más insatisfechos son tu mayor fuente de aprendizaje.'
+  },
+  {
+    'autor': 'Donald Knuth',
+    'texto': 'La programación es la habilidad de pensar en algoritmos. No se trata de escribir código; se trata de resolver problemas.'
+  },
+  {
+    'autor': 'John Carmack',
+    'texto': 'Un buen programador es alguien que siempre busca soluciones elegantes y limpias, y cuando ve un problema, lo resuelve de manera eficiente.'
+  },
+  {
+    'autor': 'Bjarne Stroustrup',
+    'texto': 'La programación es un arte, y como todas las formas de arte, mejora con la práctica.'
+  },
+  {
+    'autor': 'Alan Turing',
+    'texto': 'Las máquinas pueden hacer muchas cosas, pero la creatividad sigue siendo un reto para las computadoras.'
+  },
+  {
+    'autor': 'Kent Beck',
+    'texto': 'El diseño de software no es sólo la construcción de una solución, sino también la forma en que vas a comprender el problema.'
+  }
+];
